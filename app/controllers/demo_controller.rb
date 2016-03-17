54d1a3fcaa1b8fa2c4aad74edba15f4ab791f545
Marko Islamovic <marko@gigrove.com>
2016-03-17 19:06:31 +0000

    Added redirection

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,6 +2,20 @@ class DemoController < ApplicationController
   
   layout false 
   
-  def index
+  def  index
+    render('hello')
   end
+  
+  def hello
+    render('index')
+  end
+  
+  def other_hello
+    redirect_to(:controller => 'demo', :action => 'index')
+  end
+  
+  def lynda
+    redirect_to('http://lynda.com')
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  root "demo#index"
+  
   #get 'demo/index'
   
   match ':controller(/:action(/:id))', :via => :get
