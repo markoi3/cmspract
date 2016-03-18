3f5a4d819d675d4219a153806869116c955fcf9e
Marko Islamovic <marko@gigrove.com>
2016-03-18 18:46:40 +0000

    added params with page calculation

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,11 +3,13 @@ class DemoController < ApplicationController
   layout false 
   
   def  index
-    render('hello')
   end
   
   def hello
-    render('index')
+    #render('index')
+    @array = [1,2,3,4,5]
+    @id = params['id'].to_i
+    @page = params[:page].to_i
   end
   
   def other_hello
